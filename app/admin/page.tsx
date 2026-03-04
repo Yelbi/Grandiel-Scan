6e1870086c471db034333662e51cbbca6a5acad3
Grandiel <bgrandiel@gmail.com>
2026-03-04 19:59:48 -0400

    4/3/26

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,9 @@
-import { notFound } from 'next/navigation';
 import { getAllMangas } from '@/lib/data';
 import AdminClient from './AdminClient';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPage() {
-  // Bloqueado completamente en producción
-  if (process.env.NODE_ENV !== 'development') {
-    notFound();
-  }
-
   const mangas = await getAllMangas();
   return <AdminClient initialMangas={mangas} />;
 }
